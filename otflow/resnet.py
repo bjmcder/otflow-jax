0adaf45a7f4fcbb3a9a40840975b733d920460c1
Brian <bjmcder@gmail.com>
2023-02-24 12:10:10 -0500

    fixed functions to bitwise match original OT flow

--- otflow/resnet.py
+++ otflow/resnet.py
@@ -1,6 +1,7 @@
 import jax
 
 import equinox as eqx
+import jax.nn as jnn
 import jax.numpy as jnp
 import jax.random as jr
 
@@ -21,7 +22,8 @@ class ResNet(eqx.Module):
                  in_size: int,
                  hidden_size: int,
                  num_hidden: int = 2,
-                 seed: int = 0):
+                 seed: int = 0,
+                 test_mode = False):
         """
         Class Constructor for ResNet.
 
@@ -44,7 +46,7 @@ class ResNet(eqx.Module):
         self.hidden_dimension = hidden_size
         self.num_hidden_layers = num_hidden
 
-        self.step_size = 1.0/(self.hidden_dimension-1)
+        self.step_size = 1.0/(self.num_hidden_layers-1)
 
         self.layers = []
 
@@ -58,11 +60,24 @@ class ResNet(eqx.Module):
                                       use_bias=True,
                                       key=key)
 
+        if test_mode:
+            w0_init = jnn.initializers.constant(0.1)
+            b0_init = jnn.initializers.constant(0.2)
+
+            opening_layer = eqx.tree_at(lambda l: l.weight,
+                                        opening_layer,
+                                        w0_init(key, opening_layer.weight.shape))
+            opening_layer = eqx.tree_at(lambda l: l.bias,
+                                        opening_layer,
+                                        b0_init(key, opening_layer.bias.shape))
+
         # For the activation function, we use the antiderivative of the tanh
         # function. This helps with analytic gradients downstream in the
         # potential operator.
-        self.activation = \
+        integral_tanh = \
             lambda x: (jnp.abs(x) + jnp.log(1+jnp.exp(-2*jnp.abs(x))))
+
+        self.activation = jax.jit(integral_tanh)
 
         # Collect the opening layer
         self.layers.append(opening_layer)
@@ -71,10 +86,22 @@ class ResNet(eqx.Module):
         keys = jr.split(key, self.num_hidden_layers-1)
         for i in range(self.num_hidden_layers-1):
 
-            self.layers.append(eqx.nn.Linear(self.hidden_dimension,
+            newlayer = eqx.nn.Linear(self.hidden_dimension,
                                              self.hidden_dimension,
                                              use_bias=True,
-                                             key=keys[i]))
+                                             key=keys[i])
+
+        if test_mode:
+            w_init = jnn.initializers.constant(0.3)
+            b_init = jnn.initializers.constant(0.3)
+            newlayer = eqx.tree_at(lambda l: l.weight,
+                                   newlayer,
+                                   w_init(keys[i], newlayer.weight.shape))
+            newlayer = eqx.tree_at(lambda l: l.bias,
+                                   newlayer,
+                                    b_init(keys[i], newlayer.bias.shape))
+
+            self.layers.append(newlayer)
 
     def __call__(self, x):
         """
@@ -97,7 +124,10 @@ class ResNet(eqx.Module):
 
         A ResNet has the following functional form.
 
-        Let s be the input tensor.
+        Let x be the input tensor, consisting of data values concatenated with the
+        time t.
+
+        x = concat(data,t)
 
         u0 = activation(K0*s + b0)
         u1 = u0 + h*activation(K1*u0 + b1)
@@ -121,3 +151,97 @@ class ResNet(eqx.Module):
             y = y.at[:].add(self.step_size*self.activation(self.layers[i](y)))
 
         return y
+
+    def evaluate_layer(self, x, layer_idx):
+        """
+        Invoke the affine operation of a single layer.
+        """
+        return self.layers[layer_idx](x)
+
+    def evaluate_forward(self, x):
+        """
+        Invoke the forward pass of the model, and store all intermediate layer values.
+        """
+
+        u = jnp.zeros([self.num_hidden_layers,self.hidden_dimension])
+
+        u = u.at[0,:].set(self.activation(self.layers[0](x)))
+
+        for i in range(1, self.num_hidden_layers):
+            lay = self.layers[i](u[i-1])
+            u = u.at[i,:].set(u[i-1] + self.step_size*self.activation(lay))
+
+        return u
+
+    def evaluate_reverse(self, x, w, u):
+        """
+        Invoke the reverse pass through the model, storing all intermediate layer values.
+
+        Parameters
+        ----------
+        w : jax.numpy.ndarray
+            Vector of values being backpropagated.
+
+        u : jax.numpy.ndarray
+            Results of the forward pass
+
+        Returns
+        -------
+        jax.numpy.ndarray
+        """
+        h = self.step_size
+        n = self.num_hidden_layers
+
+        z = jnp.zeros([self.num_hidden_layers, self.hidden_dimension])
+
+        # Evaluate the layers in revese order, storing the intermediate
+        # activations in z.
+        for i in range(n-1, 0, -1):
+            if i == n-1:
+                term = w.T
+            else:
+                term = z[i+1]
+
+            K_i = self.layers[i].weight
+            dlayer = jax.nn.tanh(self.layers[i](u[i-1]))
+            hku = jnp.expand_dims(h*(K_i.T @ dlayer).T, axis=1)
+
+            new_z = (term + jnp.multiply(hku, term)).squeeze(1)
+            z = z.at[i].set(new_z)
+
+
+        # Finally, handle the input layer, which is not necessarily the same
+        # size as the hidden layers
+        K_0 = self.layers[0].weight
+        tanh_0 = jax.nn.tanh(self.evaluate_layer(x, 0))
+
+        product = K_0.T @ (tanh_0.T * z[1])
+
+        # Pad the results with zeros at the end so the dimensions match
+        pad_size = jnp.abs(self.hidden_dimension - self.input_dimension)
+
+        z = z.at[0,0:pad_size].set(product)
+
+        return z
+
+    def evaluate(self, x, w):
+        """
+        Evaluate the forward and reverse passes.
+        """
+
+        fwd = jax.jit(self.evaluate_forward)
+        rev = self.evaluate_reverse
+
+        u = fwd(x)
+        z = rev(x, w, u)
+
+        return u, z
+
+    def jac(self, x):
+        """
+        Return the Jacobian with respect to an input tensor x.
+        """
+
+        jfunc = jax.jacobian(self.forward)
+
+        return jfunc(x)
